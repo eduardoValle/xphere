67e1ff640a5b54424e13b2b9605f52752713a155
unknown <luiz.eduardo@DT-CL-8HP8M22.VRT.CORP>
2015-08-26 17:34:00 -0300

    Primeiros arquivos do projeto.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../libs/mongoose.h"
+#include "../libs/sqlite3.h"
+
+int main(void){
+
+	printf ("%s \n", "Iniciando o Projeto XPhere!!");
+	system("pause");
+}
